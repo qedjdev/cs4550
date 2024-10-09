8a128a7efc08c8a29352c62bde533d9ee69e72dd
Ben Wakefield <relaxingnoose@gmail.com>
2024-10-08 22:18:35 -0400

    4.2 dashboard

--- kanbas-react-web-app/src/Kanbas/Dashboard.tsx
+++ kanbas-react-web-app/src/Kanbas/Dashboard.tsx
@@ -4,103 +4,113 @@ export default function Dashboard() {
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
-      <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <img src="/images/react.png" width={200} height={100}/>
-          <div>
-            <Link className="wd-dashboard-course-link"
-              to="/Kanbas/Courses/1234/Home">
-              CS1234 React JS
-            </Link>
-            <p className="wd-dashboard-course-title">
-              Full Stack software developer
-            </p>
-            <Link to="/Kanbas/Courses/1234/Home"> Go </Link>
+      <h2 id="wd-dashboard-published">Published Courses (7)</h2> <hr />
+      <div id="wd-dashboard-courses" className="row">
+        <div className="row row-cols-1 row-cols-md-5 g-2">
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-2 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kanbas/Courses/1234/Home">
+                <img src="/images/react.png" width="100%" height={160} />
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">CS1234 React JS</h5>
+                  <p className="wd-dashboard-course-title card-text">
+                    Full Stack software developer
+                  </p>
+                  <button className="btn btn-primary">Go</button>
+                </div>
+              </Link>
+            </div>
           </div>
-        </div>
-        <br />
-        <div className="wd-dashboard-course">
-          <img src="/images/schizoerin.png" width={200} height={100} />
-          <div>
-            <Link className="wd-dashboard-course-link"
-              to="/Kanbas/Courses/2323/Home">
-              EECE2323
-            </Link>
-            <p className="wd-dashboard-course-title">
-              Fundamentals of Schizophrenia
-            </p>
-            <Link to="/Kanbas/Courses/2323/Home"> Don't Go </Link>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-2 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kanbas/Courses/2323/Home">
+                <img src="/images/schizoerin.png" width="100%" height={160} />
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">EECE2323</h5>
+                  <p className="wd-dashboard-course-title card-text">
+                  Fundamentals of Schizophrenia
+                  </p>
+                  <button className="btn btn-primary">Dissociate</button>
+                </div>
+              </Link>
+            </div>
           </div>
-        </div>
-        <br />
-        <div className="wd-dashboard-course">
-          <img src="/images/cs2.png" width={200} height={100} />
-          <div>
-            <Link className="wd-dashboard-course-link"
-              to="/Kanbas/Courses/2101/Home">
-              CS2 101
-            </Link>
-            <p className="wd-dashboard-course-title">
-              Wallbang Calculus
-            </p>
-            <Link to="/Kanbas/Courses/2101/Home"> 260 LJ </Link>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-2 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kanbas/Courses/2101/Home">
+                <img src="/images/cs2.png" width="100%" height={160} />
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">CS2101</h5>
+                  <p className="wd-dashboard-course-title card-text">
+                  Wallbang Calculus
+                  </p>
+                  <button className="btn btn-primary">260 LJ</button>
+                </div>
+              </Link>
+            </div>
           </div>
-        </div>
-        <br />
-        <div className="wd-dashboard-course">
-          <img src="/images/war.jpg" width={200} height={100} />
-          <div>
-            <Link className="wd-dashboard-course-link"
-              to="/Kanbas/Courses/2001/Home">
-              HIST2001
-            </Link>
-            <p className="wd-dashboard-course-title">
-              Peace in the Middle East
-            </p>
-            <Link to="/Kanbas/Courses/2001/Home"> Join Remotely via MQ-1 Predator Drone </Link>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-2 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kanbas/Courses/2001/Home">
+                <img src="/images/war.jpg" width="100%" height={160} />
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">HIST2001</h5>
+                  <p className="wd-dashboard-course-title card-text">
+                  Peace in the Middle East
+                  </p>
+                  <button className="btn btn-primary">MQ-1 Drone Feed</button>
+                </div>
+              </Link>
+            </div>
           </div>
-        </div>
-        <br />
-        <div className="wd-dashboard-course">
-          <img src="/images/2323cry.jpg" width={200} height={100} />
-          <div>
-            <Link className="wd-dashboard-course-link"
-              to="/Kanbas/Courses/0714/Home">
-              LAW0714
-            </Link>
-            <p className="wd-dashboard-course-title">
-              E-Dating Divorce Law
-            </p>
-            <Link to="/Kanbas/Courses/0714/Home"> Discord Server Invite </Link>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-2 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kanbas/Courses/0714/Home">
+                <img src="/images/2323cry.jpg" width="100%" height={160} />
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">LAW0714</h5>
+                  <p className="wd-dashboard-course-title card-text">
+                  E-Dating Divorce Law
+                  </p>
+                  <button className="btn btn-primary">Discord</button>
+                </div>
+              </Link>
+            </div>
           </div>
-        </div>
-        <br />
-        <div className="wd-dashboard-course">
-          <img src="/images/hwy9.png" width={200} height={100} />
-          <div>
-            <Link className="wd-dashboard-course-link"
-              to="/Kanbas/Courses/0009/Home">
-              CIVE0009
-            </Link>
-            <p className="wd-dashboard-course-title">
-              Building Fast Roads
-            </p>
-            <Link to="/Kanbas/Courses/0009/Home"> Zoom </Link>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-2 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kanbas/Courses/0009/Home">
+                <img src="/images/hwy9.png" width="100%" height={160} />
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">CIVE0009</h5>
+                  <p className="wd-dashboard-course-title card-text">
+                  Building Fast Roads
+                  </p>
+                  <button className="btn btn-primary">Race</button>
+                </div>
+              </Link>
+            </div>
           </div>
-        </div>
-        <br />
-        <div className="wd-dashboard-course">
-          <img src="/images/latency.png" width={200} height={100} />
-          <div>
-            <Link className="wd-dashboard-course-link"
-              to="/Kanbas/Courses/3700/Home">
-              CS3700
-            </Link>
-            <p className="wd-dashboard-course-title">
-              Networks & Distributed Systems - Teaching by Counterexample
-            </p>
-            <Link to="/Kanbas/Courses/3700/Home"> Zoom </Link>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-2 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kanbas/Courses/0009/Home">
+                <img src="/images/latency.png" width="100%" height={160} />
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">CS3700</h5>
+                  <p className="wd-dashboard-course-title card-text">
+                  Networks & Distributed Systems
+                  </p>
+                  <button className="btn btn-primary">Connect</button>
+                </div>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
--- kanbas-react-web-app/src/Kanbas/Navigation.tsx
+++ kanbas-react-web-app/src/Kanbas/Navigation.tsx
@@ -1,30 +1,51 @@
 import { AiOutlineDashboard } from "react-icons/ai";
-import { IoCalendarOutline } from "react-icons/io5";
-import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
-import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import { LiaBookSolid, LiaCalendarAltSolid, LiaCogSolid, LiaInboxSolid } from "react-icons/lia";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { Link, useLocation } from "react-router-dom";
+import "./styles.css";
+
 export default function KanbasNavigation() {
+  const location = useLocation();
+
   return (
-    <div id="wd-kanbas-navigation" style={{ width: 120 }} 
-         className="list-group rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
-      <a id="wd-neu-link" target="_blank" 
+    <div id="wd-kanbas-navigation" style={{ width: 120 }}
+      className="list-group rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
+      <a id="wd-neu-link" target="_blank"
         href="https://www.northeastern.edu/"
         className="list-group-item bg-black border-0 text-center">
         <img src="/images/NEU.png" width="75px" /></a>
+
+    
+
       <Link to="/Kanbas/Account" id="wd-account-link"
-        className="list-group-item text-center border-0 bg-black text-white">
-        <FaRegCircleUser className="fs-1 text text-white" /><br />
+        className={`list-group-item text-center border-0 bg-black text-white`}>
+        <FaRegCircleUser className={`fs-1 text-white`} /><br />
         Account </Link>
+
       <Link to="/Kanbas/Dashboard" id="wd-dashboard-link"
-        className="list-group-item text-center border-0
-                   bg-white text-danger">
-        <AiOutlineDashboard className="fs-1 text-danger" /><br />
+        className={`list-group-item text-center border-0 ${location.pathname === "/Kanbas/Dashboard" ? 'bg-white text-danger' : 'bg-black text-white'}`}>
+        <AiOutlineDashboard className={`fs-1 text-danger`} /><br />
         Dashboard </Link>
-      <Link to="/Kanbas/Dashboard" id="wd-course-link"
-        className="list-group-item text-white
-                   bg-black text-center border-0">
-        <LiaBookSolid className="fs-1 text-danger" /><br />
+
+      <Link to="/Kanbas/Courses" id="wd-course-link"
+        className={`list-group-item text-center border-0 ${location.pathname === "/Kanbas/Courses" ? 'bg-white text-danger' : 'bg-black text-white'}`}>
+        <LiaBookSolid className={`fs-1 text-danger`} /><br />
         Courses </Link>
-      {/* complete styling the rest of the links */}
+
+      <Link to="/Kanbas/Calendar" id="wd-calendar-link"
+        className={`list-group-item text-center border-0 ${location.pathname === "/Kanbas/Calendar" ? 'bg-white text-danger' : 'bg-black text-white'}`}>
+        <LiaCalendarAltSolid className={`fs-1 text-danger`} /><br />
+        Calendar </Link>
+
+      <Link to="/Kanbas/Inbox" id="wd-inbox-link"
+        className={`list-group-item text-center border-0 ${location.pathname === "/Kanbas/Inbox" ? 'bg-white text-danger' : 'bg-black text-white'}`}>
+        <LiaInboxSolid className={`fs-1 text-danger`} /><br />
+        Inbox </Link>
+
+      <Link to="/Labs" id="wd-labs-link"
+        className={`list-group-item text-center border-0 ${location.pathname === "/Labs" ? 'bg-white text-danger' : 'bg-black text-white'}`}>
+        <LiaCogSolid className={`fs-1 text-danger`} /><br />
+        Labs </Link>
     </div>
-);}
+  );
+}
