917ed6ff1615bcc4907841f06c888be3d2ec2eb9
Ben Wakefield <relaxingnoose@gmail.com>
2024-11-22 19:38:33 -0500

    3.2.2

--- kanbas-node-server-app/index.js
+++ kanbas-node-server-app/index.js
@@ -1,8 +1,7 @@
-import express from 'express';
-import Hello from "./hello.js";
+import express from 'express'
+import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
-const app = express();
-app.use(express.json());
-Lab5(app);
-Hello(app);
-app.listen(4000);
+const app = express()
+Lab5(app)
+Hello(app)
+app.listen(process.env.PORT || 4000)
--- kanbas-react-web-app/src/Landing/Navigation.tsx
+++ kanbas-react-web-app/src/Landing/Navigation.tsx
@@ -9,8 +9,9 @@ export default function Navigation() {
         <Link to="/Labs/Lab2" id="wd-labs-link">Lab 2</Link><br />
         <Link to="/Labs/Lab3" id="wd-labs-link">Lab 3</Link><br />
         <Link to="/Labs/Lab4" id="wd-labs-link">Lab 4</Link><br />
+        <Link to="/Labs/Lab5" id="wd-labs-link">Lab 5</Link><br />
       </ul>
-      <a href="https://github.com/qedjdev/cs4550/tree/a4" id="wd-a4-repo">source code</a><br />
+      <a href="https://github.com/qedjdev/cs4550/tree/a5" id="wd-a5-repo">source code</a><br />
     </div>
   );
 }
