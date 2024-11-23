5515f6394302b70a1cf391da2605cc513888ea06
Ben Wakefield <relaxingnoose@gmail.com>
2024-11-22 20:06:43 -0500

    -->3.4.7

--- kanbas-react-web-app/src/Labs/Lab5/index.tsx
+++ kanbas-react-web-app/src/Labs/Lab5/index.tsx
@@ -1,6 +1,8 @@
 import EnvironmentVariables from "./EnvironmentVariables";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
+import WorkingWithArrays from "./WorkingWithArrays";
+import WorkingWithObjects from "./WorkingWithObjects";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function Lab5() {
   return (
@@ -14,6 +16,8 @@ export default function Lab5() {
       <EnvironmentVariables />
       <PathParameters />
       <QueryParameters />
+      <WorkingWithObjects />
+      <WorkingWithArrays />
     </div>
   );
 }
